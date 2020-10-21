089137f27ca741221a9641800500a2a01395f213
Restyled.io <commits@restyled.io>
2020-10-21 14:39:21 +0000

    Restyled by shfmt

--- tde/autolock.sh
+++ tde/autolock.sh
@@ -9,19 +9,17 @@ timefour=${4:-60}
 timetwolock=${5:-mantablockscreen -sc}
 
 xidlehook \
-        --not-when-fullscreen \
-        --not-when-audio \
-        --timer "$timeone" \
-        'brightness -g > /tmp/brightness; brightness -s 5' \
-        'brightness -s $(cat /tmp/brightness)' \
-        --timer "$timetwo" \
-        "$timetwolock" \
-        'brightness -s $(cat /tmp/brightness)' \
-        --timer "$timethree" \
-        "xset dpms force off" \
-        'brightness -s $(cat /tmp/brightness)' \
-        --timer "$timefour" \
-        "systemctl suspend" \
-        'brightness -s $(cat /tmp/brightness)'
-
-
+  --not-when-fullscreen \
+  --not-when-audio \
+  --timer "$timeone" \
+  'brightness -g > /tmp/brightness; brightness -s 5' \
+  'brightness -s $(cat /tmp/brightness)' \
+  --timer "$timetwo" \
+  "$timetwolock" \
+  'brightness -s $(cat /tmp/brightness)' \
+  --timer "$timethree" \
+  "xset dpms force off" \
+  'brightness -s $(cat /tmp/brightness)' \
+  --timer "$timefour" \
+  "systemctl suspend" \
+  'brightness -s $(cat /tmp/brightness)'
--- tde/autorun.sh
+++ tde/autorun.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # MIT License
-# 
+#
 # Copyright (c) 2019 manilarome
 # Copyright (c) 2020 Tom Meyers
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,9 +26,8 @@
 userlocation="$HOME/.config/tos/autostart"
 
 if [[ -f "$HOME/.config/tos/autostart/config" ]]; then
-        userlocation=$(grep -E "location" "$HOME"/.config/tos/autostart/config | cut -d= -f2)
+  userlocation=$(grep -E "location" "$HOME"/.config/tos/autostart/config | cut -d= -f2)
 fi
-
 
 function run() {
   if ! pgrep -f "$1"; then
@@ -41,51 +40,50 @@ tos theme set "$(tos theme list | head -n1)"
 setxkbmap "$(cut -d= -f2 /etc/vconsole.conf | cut -d- -f1)"
 
 if ! pgrep tos; then
-    nohup tos theme daemon &>/dev/null &# launch a tos daemon
+  nohup tos theme daemon &>/dev/null & # launch a tos daemon
 fi
 
 if [[ "$(command -v mantablockscreen)" ]]; then
-    # create a cached image for the lock screen
-    mantablockscreen -i "$(tos theme list | head -n1)" &
+  # create a cached image for the lock screen
+  mantablockscreen -i "$(tos theme list | head -n1)" &
 fi
 
 if [[ "$(command -v psi-notify)" ]]; then
-    pgrep psi-notify &>/dev/null || psi-notify &>/dev/null &
+  pgrep psi-notify &>/dev/null || psi-notify &>/dev/null &
 fi
 
 if grep -q "bluetooth=false" ~/.config/tos/theme; then
-        bluetoothctl power off
+  bluetoothctl power off
 fi
 
 if [[ "$(command -v libinput-gestures)" ]]; then
-        libinput-gestures &
+  libinput-gestures &
 fi
 
 # launch a polkit authentication manager
 if [[ "$(command -v lxsession)" ]]; then
-        lxsession -s TOS -e TDE &
+  lxsession -s TOS -e TDE &
 fi
 
 # watch for dbus events
 process="python /etc/xdg/awesome/tos-udev-dbus.py"
 if ! pgrep -f "$process"; then
-    PYTHONDONTWRITEBYTECODE=1 "$process" & 
+  PYTHONDONTWRITEBYTECODE=1 "$process" &
 fi
 
 # autolock the system
 if [[ "$(command -v xidlehook)" ]]; then
-    sh /etc/xdg/awesome/autolock.sh &
+  sh /etc/xdg/awesome/autolock.sh &
 fi
 
 # run clipboard manager
 if [[ "$(command -v greenclip)" ]]; then
-    pgrep greenclip || greenclip daemon
+  pgrep greenclip || greenclip daemon
 fi
 
 # autostart user scripts if that directory exists
 if [[ -d "$userlocation" ]]; then
-        for script in "$userlocation"/*.sh; do
-            "$script" & # launch all user scripts in the background
-        done
+  for script in "$userlocation"/*.sh; do
+    "$script" & # launch all user scripts in the background
+  done
 fi
-
