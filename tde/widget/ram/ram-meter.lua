4da7c7e882a84a0becb8ea7bc75c3e7719f0fd41
Tom Meyers <tom@odex.be>
2020-11-24 17:44:17 +0100

    Correctly calculate the ram usage

--- tde/widget/ram/ram-meter.lua
+++ tde/widget/ram/ram-meter.lua
@@ -42,13 +42,21 @@ local slider =
 delayed_timer(
   config.ram_poll,
   function()
-    local stdout = file.lines("/proc/meminfo", nil, 3)
+    local stdout = file.lines("/proc/meminfo", nil, 24)
     if #stdout < 3 then
       return
     end
     local total = string.gmatch(stdout[1], "%d+")()
-    local free = string.gmatch(stdout[3], "%d+")()
-    local usage = (1 - (free / total)) * 100
+    local free = string.gmatch(stdout[2], "%d+")()
+    local buffer = string.gmatch(stdout[4], "%d+")()
+    local cache = string.gmatch(stdout[5], "%d+")()
+    local sReclaimable = string.gmatch(stdout[24], "%d+")()
+
+    -- the used ram in kB
+    local used = total - free - buffer - cache - sReclaimable
+
+    -- the usage in percent
+    local usage = (used / total) * 100
     slider:set_value(usage)
     signals.emit_ram_usage(usage)
     signals.emit_ram_total(total)
--- tests/profile-scripts/ram-value.lua
+++ tests/profile-scripts/ram-value.lua
@@ -5,14 +5,21 @@ require("lib-tde.luapath")
 local file = require("lib-tde.file")
 
 function get_ram_usage()
-    local stdout = file.lines("/proc/meminfo", nil, 3)
+    local stdout = file.lines("/proc/meminfo", nil, 24)
     if #stdout < 3 then
         return
     end
     local total = string.gmatch(stdout[1], "%d+")()
-    local free = string.gmatch(stdout[3], "%d+")()
-    local usage = (1 - (free / total)) * 100
+    local free = string.gmatch(stdout[2], "%d+")()
+    local buffer = string.gmatch(stdout[4], "%d+")()
+    local cache = string.gmatch(stdout[5], "%d+")()
+    local sReclaimable = string.gmatch(stdout[24], "%d+")()
+
+    local used = total - free - buffer - cache - sReclaimable
+
+    local usage = (used / total) * 100
     print("Ram usage: " .. usage .. "%")
+    print("Ram used: " .. used .. "kB")
     print("Ram total: " .. total .. "kB")
 end
 
