0682a39962f503638845fc3508bcee5f9481f70c
Tom Meyers <tom@odex.be>
2020-12-02 21:32:21 +0100

    Easily print out tables and other types

--- tde/lib-tde/logger.lua
+++ tde/lib-tde/logger.lua
@@ -63,18 +63,60 @@ if filehandle.dir_exists(dir) then
 	filehandle.overwrite(filename, "")
 end
 
--- open the log file (if no handle exists already)
+-- helper function to convert a table to a string
+-- WARN: For internal use only, this should never be exposed to the end user
+-- usage: local string = pretty_print_table({})
+local function table_to_string(tbl, depth, indent)
+	indent = indent or 1
+	-- limit the max size
+	if indent > depth then
+		if type(tbl) == "table" then
+			return "{  ..."
+		end
+		return ""
+	end
+	local formatting = string.rep("  ", indent)
+	local result = "{\n"
+	for k, v in pairs(tbl) do
+		local format = formatting .. tostring(k) .. ": "
+		if type(v) == "table" then
+			result = result .. format .. table_to_string(v, depth - 1, indent + 1) .. formatting .. "},\n"
+		else
+			if type(v) == "string" then
+				result = result .. format .. "'" .. v .. "',\n"
+			else
+				result = result .. format .. tostring(v) .. ",\n"
+			end
+		end
+	end
+	-- edge case initial indentation requires manually adding ending bracket
+	if indent == 1 then
+		return result .. "}"
+	end
+	return result
+end
 
 --- The default lua print message is overriden
 -- @tparam string arg The string to print
 -- @tparam[opt] enum log_type The type of logmessage
+-- @tparam[opt] depth number In case you print a table, show the table until a certain depth
 -- @staticfct print
 -- @usage -- Print the error message "hello"
 -- print("hello", logger.error)
-print = function(arg, log_type)
+print = function(arg, log_type, depth)
+	depth = depth or 3
+	-- validate the input
 	if arg == nil then
 		return
 	end
+	if type(arg) == "table" then
+		arg = table_to_string(arg, depth)
+	end
+	if not (type(arg) == "string") then
+		arg = tostring(arg)
+	end
+
+	-- effective logging
 	local file = io.open(filename, "a")
 
 	local log = log_type or LOG_INFO
