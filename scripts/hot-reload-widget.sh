98a21c2690edf36c045f80eeecda2c6516643f6c
Restyled.io <commits@restyled.io>
2020-12-31 13:23:22 +0000

    Restyled by shfmt

--- scripts/hot-reload-widget.sh
+++ scripts/hot-reload-widget.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # MIT License
-# 
+#
 # Copyright (c) 2019 manilarome
 # Copyright (c) 2020 Tom Meyers
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,7 +37,7 @@ UPDATE_SPEED="5"
 # reload as fast as possible
 FAST="0"
 
-function print_help(){
+function print_help() {
   name=$(basename "$0" .sh)
   echo -e "$name : OPTIONS [s] <lua file>"
   echo ""
@@ -48,92 +48,92 @@ function print_help(){
 
 while true; do
   case "$1" in
-    "-s"|"--side")
-        FAST="1"
-        shift
-    ;;
-    "-h"|"--help")
-        print_help
-        exit 0
-    ;;
+    "-s" | "--side")
+      FAST="1"
+      shift
+      ;;
+    "-h" | "--help")
+      print_help
+      exit 0
+      ;;
     "")
-     [[ "$LUA_FILE" == "" ]] && print_help
-     break
-    ;;
+      [[ "$LUA_FILE" == "" ]] && print_help
+      break
+      ;;
     **)
-        LUA_FILE="$1"
-        shift
-    ;;
+      LUA_FILE="$1"
+      shift
+      ;;
   esac
 done
 
-function notify_user(){
-    echo "$1"
-    notify-send "TDE widget hot-reload" "$1"
+function notify_user() {
+  echo "$1"
+  notify-send "TDE widget hot-reload" "$1"
 }
 
-function notify_user_error(){
-    echo "$1"
-    notify-send "TDE widget hot-reload" "$1" -u "critical"
+function notify_user_error() {
+  echo "$1"
+  notify-send "TDE widget hot-reload" "$1" -u "critical"
 }
 
-function check_state(){
-    # check if the widget exists
-    if [[ ! -f "$LUA_FILE" ]]; then
-        notify_user "Please supply a file to hot-reload (must be valid lua)"
-        exit 1
+function check_state() {
+  # check if the widget exists
+  if [[ ! -f "$LUA_FILE" ]]; then
+    notify_user "Please supply a file to hot-reload (must be valid lua)"
+    exit 1
+  fi
+  if [[ ! "$(command -v inotifywait)" ]]; then
+    if [[ ! "$(command -v tos)" ]]; then
+      notify_user "Can't provide hot reload support, please install inotify-tools"
+      exit 1
     fi
-    if [[ ! "$(command -v inotifywait)" ]]; then
-        if [[ ! "$(command -v tos)" ]]; then
-         notify_user "Can't provide hot reload support, please install inotify-tools"
-         exit 1  
-        fi
-        notify_user "Queing inotify-tools using <span  weight='bold'>tos -S inotify-tools</span>\n this is to support hot-reloading"
-        tos -S inotify-tools --noconfirm
-    fi
-    # start dev-widget-hot-reload tde daemon if it doesn't exist yet
-    if [[ "$FAST" == "1" ]]; then
-        tde-client "if not _G.dev_widget_side_view_started then _G.dev_widget_side_view_init() end"
-    else
-        tde-client "if not _G.dev_widget_started then _G.dev_widget_init() end"
-    fi
+    notify_user "Queing inotify-tools using <span  weight='bold'>tos -S inotify-tools</span>\n this is to support hot-reloading"
+    tos -S inotify-tools --noconfirm
+  fi
+  # start dev-widget-hot-reload tde daemon if it doesn't exist yet
+  if [[ "$FAST" == "1" ]]; then
+    tde-client "if not _G.dev_widget_side_view_started then _G.dev_widget_side_view_init() end"
+  else
+    tde-client "if not _G.dev_widget_started then _G.dev_widget_init() end"
+  fi
 
 }
 
-function hot_reload(){
-    file="$(mktemp /tmp/tde_widget_hot_reload_XXXXXXXXX.lua)"
-    cp "$LUA_FILE" "$file"
-    name="$(echo "$file" | sed 's/.lua$//g')"
+function hot_reload() {
+  file="$(mktemp /tmp/tde_widget_hot_reload_XXXXXXXXX.lua)"
+  cp "$LUA_FILE" "$file"
+  name="$(echo "$file" | sed 's/.lua$//g')"
 
-    if [[ "$FAST" == "1" ]]; then
-        result="$(tde-client "_G.dev_widget_side_view_refresh(\"$name\")")"
-    else
-        result="$(tde-client "_G.dev_widget_refresh(\"$name\")")"
-    fi
+  if [[ "$FAST" == "1" ]]; then
+    result="$(tde-client "_G.dev_widget_side_view_refresh(\"$name\")")"
+  else
+    result="$(tde-client "_G.dev_widget_refresh(\"$name\")")"
+  fi
 
-    if [[ -n "$result" ]]; then
-        notify_user_error "$(echo "$result" | sed -e 's/^ *[a-z]* "//' -e 's/"$//' -e "s;$file;$LUA_FILE;")"
-    fi
-    notify_user "💡 Hot Reloaded $LUA_FILE"
-    rm "$file"
+  if [[ -n "$result" ]]; then
+    notify_user_error "$(echo "$result" | sed -e 's/^ *[a-z]* "//' -e 's/"$//' -e "s;$file;$LUA_FILE;")"
+  fi
+  notify_user "💡 Hot Reloaded $LUA_FILE"
+  rm "$file"
 }
 
-function listen(){
-    notify_user "Starting listening to filesystem events for $LUA_FILE"
+function listen() {
+  notify_user "Starting listening to filesystem events for $LUA_FILE"
 
-    current_time="$(( $(date +%s) - $UPDATE_SPEED ))"
+  current_time="$(($(date +%s) - $UPDATE_SPEED))"
 
-    hot_reload
+  hot_reload
 
-    inotifywait -m -e modify "$LUA_FILE" | 
+  inotifywait -m -e modify "$LUA_FILE" |
     while read file op; do
-        # only reload every $UPDATE_SPEED seconds
-        # otherwise we get overloaded with updates
-        # which is taxing on the system
-        if [[ "$(date +%s)" -gt "$(( $current_time + $UPDATE_SPEED ))" || "$FAST" == "1" ]]; then
-            hot_reload
-            current_time="$(date +%s)"
-        fi
+      # only reload every $UPDATE_SPEED seconds
+      # otherwise we get overloaded with updates
+      # which is taxing on the system
+      if [[ "$(date +%s)" -gt "$(($current_time + $UPDATE_SPEED))" || "$FAST" == "1" ]]; then
+        hot_reload
+        current_time="$(date +%s)"
+      fi
     done
 }
 
