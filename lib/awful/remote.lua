7b8bae4938eee65063b4b8b98df987701513e763
Tom Meyers <tom@odex.be>
2021-01-15 21:54:55 +0100

    Make the repl for tables better

--- lib/awful/remote.lua
+++ lib/awful/remote.lua
@@ -16,6 +16,37 @@ local unpack = unpack or table.unpack -- luacheck: globals unpack (compatibility
 local dbus = dbus
 local type = type
 
+local function table_to_string(tbl, depth, indent)
+    depth = depth or 2
+    indent = indent or 1
+    -- limit the max size
+    if indent > depth then
+        if type(tbl) == "table" then
+            return "{  ..."
+        end
+        return ""
+    end
+    local formatting = string.rep("  ", indent)
+    local result = "{\n"
+    for k, v in pairs(tbl) do
+        local format = formatting .. tostring(k) .. ": "
+        if type(v) == "table" then
+            result = result .. format .. table_to_string(v, depth - 1, indent + 1) .. formatting .. "},\n"
+        else
+            if type(v) == "string" then
+                result = result .. format .. "'" .. v .. "',\n"
+            else
+                result = result .. format .. tostring(v) .. ",\n"
+            end
+        end
+    end
+    -- edge case initial indentation requires manually adding ending bracket
+    if indent == 1 then
+        return result .. "}"
+    end
+    return result
+end
+
 if dbus then
     dbus.connect_signal(
         "org.awesomewm.awful.Remote",
@@ -38,6 +69,9 @@ if dbus then
                     elseif t == "number" then
                         table.insert(retvals, "d")
                         table.insert(retvals, v)
+                    elseif t == "table" then
+                        table.insert(retvals, "s")
+                        table.insert(retvals, table_to_string(v))
                     else
                         table.insert(retvals, "s")
                         table.insert(retvals, tostring(v))
