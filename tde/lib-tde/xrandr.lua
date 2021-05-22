d8003516b3fcfc44c7ec7dfc9d938b9300bec858
Tom <tom@odex.be>
2021-05-22 14:51:54 +0200

    LIB-TDE: Add documentation to xrandr
    Add documentation to the required items,
    Explain the functino/logic of certain elements

--- tde/lib-tde/xrandr.lua
+++ tde/lib-tde/xrandr.lua
@@ -22,6 +22,19 @@
 --OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 --SOFTWARE.
 ]]
+
+---------------------------------------------------------------------------
+-- Lua bindings for the xrandr interface/cli
+--
+-- xrandr is an official configuration utility to the RandR (Resize and Rotate) X Window System extension.
+-- It can be used to set the size, orientation or reflection of the outputs for a screen.
+--
+--
+-- @author Tom Meyers
+-- @copyright 2020 Tom Meyers
+-- @tdedatamod lib-tde.xrandr
+---------------------------------------------------------------------------
+
 -- Separating Multiple Monitor functions as a separated module (taken from awesome wiki)
 local gtable = require("gears.table")
 
@@ -33,7 +46,11 @@ local LOG_ERROR = "\27[0;31m[ ERROR "
 -- A path to a fancy icon
 local icon_path = "/etc/xdg/tde/theme/icons/laptop.svg"
 
--- Get active outputs
+--- Get the active screen outputs
+-- @treturn table A list containing the names of all screens
+-- @staticfct lib-tde.xrandr.outputs
+-- @usage -- Get all active screens
+-- lib-tde.xrandr.outputs() -- Returns (For example) {'eDP1', 'DP1', 'DP2'}
 local function outputs()
    local output_tbl = {}
    local xrandr = io.popen("xrandr -q --current")
@@ -51,6 +68,12 @@ local function outputs()
    return output_tbl
 end
 
+--- Arrange all possible permutations of a given list of screens
+-- @tparam table list A list of screen outputs
+-- @treturn table A list containing all screen permutations
+-- @staticfct lib-tde.xrandr.arrange
+-- @usage -- Get all list of all permutations
+-- lib-tde.xrandr.arrange({'eDP1', 'DP2'}) -- Returns {1: {'eDP1'}, 2: {'DP2'}, 3: {'eDP1', 'DP2'}, 4: {'DP2', 'eDP1'}}
 local function arrange(out)
    -- We need to enumerate all permutations of horizontal outputs.
 
@@ -76,7 +99,11 @@ local function arrange(out)
    return choices
 end
 
--- Build available choices
+--- Build available screen orientations with accompanying xrandr commands
+-- @treturn table A list of tables with the following signature: {label, cmd, choice}
+-- @staticfct lib-tde.xrandr.menu
+-- @usage -- Get all list of all permutations with labels, command and choice
+-- lib-tde.xrandr.menu({'eDP1', 'DP2'})
 local function menu()
    local menu_tbl = {}
    local out = outputs()
@@ -143,6 +170,9 @@ local function naughty_destroy_callback(_)
    end
 end
 
+-- This should only be shown in the (keys) global.lua file
+-- Don't expose it to the plugin api, as it has state that changes on each call
+-- See the `state` variable
 local function xrandr()
    -- Build the list of choices
    if not state.index then
@@ -179,6 +209,11 @@ local function xrandr()
    state.cid = noti.id
 end
 
+--- Get a list of all existing screens with their resolutions and refresh rates
+-- @treturn table A table of tables with the following signature: {'eDP1': {resolution: {'Refresh Rate 1', 'Refresh Rate 2', ...}, '1920x1080': {'60'}, ...}, ...}
+-- @staticfct lib-tde.xrandr.output_data
+-- @usage -- Get a list of all screens
+-- local xrandr_data = lib-tde.xrandr.output_data()
 local function output_data()
    -- get the current screen output
    local xrandr_out = io.popen("xrandr -q --current")
@@ -219,6 +254,13 @@ local function output_data()
    return data_tbl
 end
 
+--- Find the highest resolution of a given monitor with all possible refresh rates
+-- @tparam table|string output A screen table from lib-tde.xrandr.output_data or the string representation of an randr screen
+-- @treturn string A string representation of the highest resolution of a given monitor eg 1920x1080
+-- @treturn table The seconds return element is a table of possible refresh rates
+-- @staticfct lib-tde.xrandr.highest_resolution
+-- @usage -- Get back the highest resolution of the 'eDP1' monitor
+-- local xrandr_data = lib-tde.xrandr.highest_resolution('eDP1') -- Returns for example: 1920x1080, {'60', '120', '144'}
 local function highest_resolution(output)
    if output == nil or (type(output) ~= "string" and type(output) ~= "table") then
       return
@@ -251,6 +293,14 @@ local function highest_resolution(output)
    return largest_res_key, lookup_table[largest_res_key]
 end
 
+--- Find the highest refresh rate of a given monitor with the accompanying resolution
+-- @tparam table|string output A screen table from lib-tde.xrandr.output_data or the string representation of an randr screen
+-- @treturn number The highest refresh rate found in Hertz
+-- @treturn string The seconds return element is a string representation of the resolution linked to this refresh rate
+-- @treturn table The third return element is a table of possible refresh rates linked to the resolution
+-- @staticfct lib-tde.xrandr.highest_refresh_rate
+-- @usage -- Get back the highest refresh rate of the 'eDP1' monitor
+-- local xrandr_data = lib-tde.xrandr.highest_refresh_rate('eDP1') -- Returns for example: 144, '1920x1080', {'60', '120', '144'}
 local function highest_refresh_rate(output)
    if output == nil or (type(output) ~= "string" and type(output) ~= "table") then
       return
@@ -271,38 +321,41 @@ local function highest_refresh_rate(output)
 
    local res_key = '1x1'
    local largest_refresh_value = 0
-   local largest_refresh_index = 0
 
    for res, value in pairs(lookup_table) do
-      for index, refresh_rate in ipairs(value) do
+      for _, refresh_rate in ipairs(value) do
          refresh_rate = tonumber(refresh_rate) or 0
          if refresh_rate > largest_refresh_value then
-            largest_refresh_index = index
             largest_refresh_value = refresh_rate
             res_key = res
          end
       end
    end
-   return largest_refresh_value, res_key, largest_refresh_index, lookup_table[res_key]
+   return largest_refresh_value, res_key, lookup_table[res_key]
 end
 
+--- Find the highest refresh rate of a given resolution
+-- @tparam table res_tbl A resolution table from lib-tde.xrandr.output_data, the reolsution table must be taken from a given screen
+-- @treturn number The highest refresh rate found in Hertz
+-- @staticfct lib-tde.xrandr.highest_refresh_rate_from_resolution
+-- @usage -- Get back the highest refresh rate of the 'eDP1' monitor with resolution '1920x1080'
+-- local xrandr_data = lib-tde.xrandr.highest_refresh_rate_from_resolution(output['eDP1']['1920x1080']) -- Returns for example 144
+-- local xrandr_data = lib-tde.xrandr.highest_refresh_rate_from_resolution({'60', '120', 144'}) -- Returns for example 144
 local function highest_refresh_rate_from_resolution(res_tbl)
    if type(res_tbl) ~= "table" then
       return
    end
 
    local largest_rate = 0
-   local largest_index = 0
 
-   for index, rate in ipairs(res_tbl) do
+   for _, rate in ipairs(res_tbl) do
       rate = tonumber(rate) or 0
       if rate > largest_rate then
          largest_rate = rate
-         largest_index = index
       end
    end
 
-   return largest_rate, largest_index
+   return largest_rate
 end
 
 return {
