c2a9ac33b0f24bec78db7bb0b28c451b3d30252d
Tom <tom@odex.be>
2021-06-08 14:34:17 +0200

    MISC: Add support for creating git releases from the shell
    We have created a nice wrapper script to create new releases for each
    time a new feature is added

---
+++ build-utils/update-release.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+VERSION="$(git rev-list --count HEAD)"
+BRANCH="$(git branch | grep '*' | cut -d' ' -f2)"
+
+
+MESSAGE="Releasing new version $BRANCH-$VERSION\nNews Link: https://github.com/ODEX-TOS/tos-desktop-environment/blob/$BRANCH/NEWS.md"
+
+# get repo name and owner
+REPO_NAME="tos-desktop-enviroment"
+REPO_OWNER="ODEX-TOS"
+
+if [[ "$(command -v gh)" == "" ]]; then
+    echo "Install github cli first"
+    echo "Then run gh auth login to authenticate"
+    exit 1
+fi
+
+
+# create the tag of this release and push it
+git tag "$BRANCH-$VERSION"
+git push origin "$BRANCH-$VERSION"
+
+
+cd build-utils || exit 1
+rm tde-* || true
+cp PKGBUILD PKGBUILD_BLANK
+
+sed -i -E 's/BRANCH=".*"/BRANCH="'$BRANCH'"/g' PKGBUILD
+
+# build the package
+makepkg -f || exit 1
+
+# cleanup build
+mv PKGBUILD_BLANK PKGBUILD
+rm -rf tos-desktop-enviroment src pkg || true
+cd ../ || exit 1
+
+file="$(find . -type f -iname 'tde-*.pkg.*' | head -n1)"
+gh release create "$BRANCH-$VERSION" -F NEWS.md $file -R "$REPO_OWNER/$REPO_NAME"
