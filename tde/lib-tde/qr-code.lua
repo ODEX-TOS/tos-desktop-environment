a8d1d7616c087b1ec8ef8f360633a6d4905d6cce
Tom <tom@odex.be>
2022-05-01 13:16:19 +0200

    LIB-TDE: QR Code generator - Add support for injecting a logo
    You can now embed a login in the qr code

--- tde/lib-tde/qr-code.lua
+++ tde/lib-tde/qr-code.lua
@@ -59,8 +59,38 @@ local function draw_cell(cr, x, y, width, height, color)
     cr:fill()
 end
 
-local function qr_to_cairo(qr, size)
+local function white_if_center(color, x, y, size, logo_size_in_blocks)
+    local center = size/2
+
+    if x < center + logo_size_in_blocks and x > center - logo_size_in_blocks and y < center + logo_size_in_blocks and y > center - logo_size_in_blocks then
+        -- numbers less than 0 are white
+        return -1
+    end
+
+    return color
+end
+
+local function draw_image_to_cairo(cr, image, x, y, width, height)
+    -- Make sure the image reference doesn't get modified in other places because of the cache
+    local surface = gears.surface.duplicate_surface(gears.surface(image))
+
+    -- scale the surface
+    cr:translate(x,y)
+    cr:scale(width/surface:get_width(), height/surface:get_height())
+    cr:translate(-x,-y)
+    cr:fill()
+    cr:set_source_surface(surface, x, y)
+    cr:paint()
+
+    surface:finish()
+end
+
+local function qr_to_cairo(qr, size, logo)
     size =  size or 100
+    local with_logo = logo ~= nil
+
+    -- The size of the logo in percentage
+    local logo_size = 10
 
     local img = cairo.ImageSurface.create(cairo.Format.ARGB32, size, size)
 
@@ -68,10 +98,22 @@ local function qr_to_cairo(qr, size)
 
     local cell_size = size / #qr
 
+    local logo_size_in_blocks = #qr * (logo_size / 100)
+
+
     for x, column in ipairs(qr) do
         for y, color in ipairs(column) do
+            if with_logo then
+                color = white_if_center(color, x, y, #qr, logo_size_in_blocks)
+            end
             draw_cell(cr, (x-1) * cell_size, (y-1) * cell_size, cell_size+1, cell_size+1, color )
         end
+    end
+
+    if with_logo then
+        local _center = size/2
+        local _logo_size = (logo_size_in_blocks * cell_size)*2
+        draw_image_to_cairo(cr, logo, _center - (_logo_size/2) , _center - (_logo_size/2), _logo_size,  _logo_size)
     end
 
     return img
@@ -95,10 +137,10 @@ end
 -- @staticfct surface
 -- @usage
 --    lib-tde.qr-code.surface("This is some text", dpi(300)) -- returns a cairo surface
-local function surface(text, size)
+local function surface(text, size, logo)
     size = size or dpi(300)
     local qr = list(text)
-    return qr_to_cairo(qr, size)
+    return qr_to_cairo(qr, size, logo)
 end
 
 --- Returns a wibox.widget.imagebox containing the qr code
@@ -107,8 +149,8 @@ end
 -- @staticfct image
 -- @usage
 --    lib-tde.qr-code.image("This is some text", dpi(300)) -- returns a wibox.widget.imagebox
-local function image(text, size)
-    return wibox.widget.imagebox(surface(text,size), false)
+local function image(text, size, logo)
+    return wibox.widget.imagebox(surface(text, size, logo), false)
 end
 
 
